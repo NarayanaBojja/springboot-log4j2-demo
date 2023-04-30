2224ccdd34cb4dbd257dba6f09442d4ad4fe832d
Narayana <narayana.bojja143@gmail.com>
2023-04-30 17:18:17 +0530

    first commit

---
+++ src/main/java/com/log4j2/demo/SpringbootLog4j2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.log4j2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLog4j2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLog4j2DemoApplication.class, args);
+	}
+
+}
